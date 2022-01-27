713b4780fa9f25d852b3d697d9a054ee73f15b7e
Gustavo Emanuel <gustavo.emanuel01@outlook.com>
2022-01-27 17:55:10 -0300

    fix to add svg teste

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react'
+import  styled from 'styled-components';
+import traco from './images/traco.svg'
+
+export const Imagem = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+ 
+
 function App() {
+
+
   return (
     <div>
-      <p>teste</p>
+      <p>Teste de colocar traço</p>
+      <Imagem src={traco} alt="" />
     </div>
   );
 }
 
 export default App;
+ 
